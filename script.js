15a0806e7e25d89537d72e9fb54a0a358753319d
kent <kent.26pg@gmail.com>
2026-06-11 11:24:32 +0200

    Add clamp function to keep box inside window

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const box = document.getElementById('movable-box');
 const BOX = 48;
 const STEP = 24;
 let x = 0, y = 0;
- 
+
+function clamp(val, min, max) {
+  return val < min ? min : val > max ? max : val;
+}
+
